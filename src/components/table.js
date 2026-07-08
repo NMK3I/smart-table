3f284fc9facbf2f5acabe5e6dbeed0a814e78ed1
Roman Girchev <girchevroman@gmail.com>
2026-07-09 01:30:08 +0300

    Version 1.0

--- src/components/table.js
+++ src/components/table.js
@@ -12,12 +12,53 @@ export function initTable(settings, onAction) {
     const root = cloneTemplate(tableTemplate);
 
     // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
-
+    if (before) {
+        before.reverse().forEach(subName => {
+            root[subName] = cloneTemplate(subName);
+            root.container.prepend(root[subName].container);
+        });
+    };
+    if (after) {
+        after.forEach(subName => {
+            root[subName] = cloneTemplate(subName);
+            root.container.append(root[subName].container);
+        });
+    };
     // @todo: #1.3 —  обработать события и вызвать onAction()
+    root.container.addEventListener('change', (e) => {
+        onAction();
+    });
+
+    root.container.addEventListener('reset', (e) => {
+        setTimeout(onAction);
+    });
+
+    root.container.addEventListener('submit', (e) => {
+        e.preventDefault();
+        onAction(e.submitter);
+    })
 
     const render = (data) => {
         // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = [];
+        const nextRows = data.map(item => {
+            const row = cloneTemplate(rowTemplate);
+
+            Object.keys(item).forEach(key => {
+                const element = row.elements[key];
+
+                if (element) {
+                    if (element.tagName === 'INPUT' || element.tagName === 'SELECT') {
+                        element.value = item[key];
+                    }
+
+                    else {
+                        element.textContent = item[key];
+                    }
+                }    
+            })
+        return row.container;
+        console.log(row.container)
+        });
         root.elements.rows.replaceChildren(...nextRows);
     }
 
--- src/main.js
+++ src/main.js
@@ -7,6 +7,11 @@ import {initData} from "./data.js";
 import {processFormData} from "./lib/utils.js";
 
 import {initTable} from "./components/table.js";
+
+import {initSearching} from './components/searching.js';
+import {initSorting} from './components/sorting.js';
+import {initPagination} from './components/pagination.js';
+import {initFiltering} from './components/filtering.js';
 // @todo: подключение
 
 
@@ -19,9 +24,13 @@ const {data, ...indexes} = initData(sourceData);
  */
 function collectState() {
     const state = processFormData(new FormData(sampleTable.container));
+    const rowsPerPage = parseInt(state.rowsPerPage);
+    const page = parseInt(state.page ?? 1);
 
     return {
-        ...state
+        ...state,
+        rowsPerPage,
+        page
     };
 }
 
@@ -33,6 +42,10 @@ function render(action) {
     let state = collectState(); // состояние полей из таблицы
     let result = [...data]; // копируем для последующего изменения
     // @todo: использование
+    result = applySearching(result, state, action);
+    result = applyFiltering(result, state, action);
+    result = applySorting(result, state, action);
+    result = applyPagination(result, state, action);
 
 
     sampleTable.render(result)
@@ -41,12 +54,33 @@ function render(action) {
 const sampleTable = initTable({
     tableTemplate: 'table',
     rowTemplate: 'row',
-    before: [],
-    after: []
+    before: ['search', 'header', 'filter'],
+    after: ['pagination']
 }, render);
 
 // @todo: инициализация
+const applyPagination = initPagination(
+    sampleTable.pagination.elements,
+    (el, page, isCurrent) => {
+        const input = el.querySelector('input');
+        const label = el.querySelector('span');
+        input.value = page;
+        input.checked = isCurrent;
+        label.textContent = page;
+        return el;
+    }
+);
 
+const applySorting = initSorting([
+    sampleTable.header.elements.sortByDate,
+    sampleTable.header.elements.sortByTotal
+]);
+
+const applyFiltering = initFiltering(sampleTable.filter.elements, {
+    searchBySeller: indexes.sellers
+})
+
+const applySearching = initSearching('search');
 
 const appRoot = document.querySelector('#app');
 appRoot.appendChild(sampleTable.container);
